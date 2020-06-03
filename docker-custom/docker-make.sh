66ad47fd9a679a42f136fc0656652b33e9258c7d
Paul Wieland <p@ul.sickdimension.com>
2020-06-03 12:01:35 -0400

    stretch-slim with sap sdk

--- docker-custom/docker-make.sh
+++ docker-custom/docker-make.sh
@@ -5,12 +5,14 @@ echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
+export OS="stretch-slim" # alpine | stretch-slim
+
 docker build --no-cache \
     --build-arg ARCH=amd64 \
     --build-arg NODE_VERSION=12 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
-    --build-arg OS=alpine \
+    --build-arg OS=$OS \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
-    --file Dockerfile.custom \
-    --tag testing:node-red-build .
+    --file Dockerfile.$OS.custom \
+    --tag pwieland/gto:saprfc-node-red-build .
